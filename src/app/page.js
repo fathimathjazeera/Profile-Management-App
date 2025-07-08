2e0479e72476bd98b260a78f7353f6cfba29d2c7
Fathimath Jazeera <jezijazeera@gmail.com>
2025-07-08 18:03:08 +0530

    designed profile card

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
+import profiles from "@/components/data";
 import Navbar from "@/components/Navbar";
+import ProfileCard from "@/components/ProfileCard";
 
 
 export default function Home() {
   return (
     <main >
     <Navbar />
+    <ProfileCard profiles={profiles} />
 
     </main>
   );
---
+++ src/components/data.js
@@ -0,0 +1,55 @@
+const profiles = [
+    {
+      id: '1',
+      name: 'Asha Thomas',
+      avatar: '/images/asha.jpg',
+      age:25,
+      gender:'Male',
+      bio: 'Frontend Developer at XYZ',
+    },
+    {
+      id: '2',
+      name: 'Rahul Menon',
+      avatar: '/images/rahul.jpg',
+      age:25,
+      gender:'Male',
+      bio: 'Backend Developer at ABC',
+    },
+    {
+      id: '3',
+      name: 'Meera Nair',
+      avatar: '/images/meera.jpg',
+      age:25,
+      gender:'Male',
+      bio: 'UX Designer at DEF',
+    },
+    {
+        id: '4',
+        name: 'Asha Thomas',
+        avatar: '/images/asha.jpg',
+        age:25,
+        gender:'Female',
+        bio: 'Frontend Developer at XYZ',
+      },
+      {
+        id: '5',
+        name: 'Rahul Menon',
+        avatar: '/images/rahul.jpg',
+        age:25,
+        gender:'Male',
+        bio: 'Backend Developer at ABC',
+  
+      },
+      {
+        id: '6',
+        name: 'Meera Nair',
+        avatar: '/images/meera.jpg',
+        age:25,
+        gender:'Male',
+        bio: 'UX Designer at DEF',
+  
+      },
+  ];
+  
+  export default profiles;
+  
